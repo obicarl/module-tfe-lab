cee0c01a5a1d32fd89bb3a362ac5ae3f89424219
Obinna Okpokwasili <limitless@obinnas-mbp-2.lan>
2018-09-09 12:53:07 -0500

    ruunning plan on its own

--- variables.tf
+++ variables.tf
@@ -5,38 +5,32 @@
 # AWS specific variables
 variable "region" {
   description = "AWS Region to which the servers should be provisioned"
-  type = "string"
 }
 
 variable "subnet_id" {
   description = "AWS Subnet to which the host should be provisioned."
-  type = "string"
 }
 
 variable "security_groups" {
   description = "Comma delimited string of AWS Security groups"
-  type = "list"
 }
 
 variable "key_name" {
   description = "Existing AWS KeyPair name.  Must match the KeyPair referenced in key_file_path"
-  type = "string"
 }
 
 variable "key_file_path" {
   description = "Location of the local private key file for the EC2 instance."
-  type = "string"
 }
 
 variable "domain_name" {
   description = "domain_name"
-  type = "string"
 }
 
 # Linux Username to leverage when connecting to the remote host
 variable "linux_username" {
   description = "Linux Instance Username"
-  type = "string"
+
 }
 
 variable "disk_sizes" {
@@ -51,22 +45,18 @@ variable "lin_mount_points" {
 
 variable "type" {
   description = "Ebs volume type"
-  type = "string"
 }
 
 # Host specific details
 
 variable "linux_password" {
   description = "linux_password"
-  type = "string"
 }
 
 variable "node_name" {
   description = "Assigned Node name for the host"
-  type = "string"
 }
 
 variable "redhat_ami" {
   description = "AWS Instance AMI"
-  type = "string"
 }
